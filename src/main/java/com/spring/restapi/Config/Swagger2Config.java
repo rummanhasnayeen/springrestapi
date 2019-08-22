51c8c201871d3da1994eff0002637692220f4b70
DSi <rummanhasnayeen94@gmail.com>
2019-08-22 22:55:39 +0600

    Swagger implemented for the application

---
+++ src/main/java/com/spring/restapi/Config/Swagger2Config.java
@@ -0,0 +1,59 @@
+package com.spring.restapi.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Config implements WebMvcConfigurer{
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors
+                        .basePackage("com.spring.restapi"))
+                .paths(PathSelectors.regex("/.*"))
+                .build().apiInfo(apiEndPointsInfo());
+    }
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("Spring Boot REST api")
+                .description("Spring boot REST API")
+                .contact(new Contact("Spring Rest", "https://restapi.com", "restapi@boot.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
+    }
+    public Contact getContactInfo(){
+        String name="Spring Rest";
+        String url="https://restapi.com";
+        String email="restapi@boot.com";
+        return new Contact(name,url,email);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/api/v2/api-docs", "/v2/api-docs");
+        registry.addRedirectViewController("/api/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
+        registry.addRedirectViewController("/api/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
+        registry.addRedirectViewController("/api/swagger-resources", "/swagger-resources");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/api/swagger-ui.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+        registry.addResourceHandler("/api/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
+
--- src/main/java/com/spring/restapi/Controller/UserController.java
+++ src/main/java/com/spring/restapi/Controller/UserController.java
@@ -2,25 +2,28 @@ package com.spring.restapi.Controller;
 
 import com.spring.restapi.Entity.User;
 import com.spring.restapi.Service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
+@Api(value="User", description="User profile REST api end-points with description")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    //End point for creating a new user
+    @ApiOperation(value = "End point for creating a new user")
     @PostMapping("/user")
     public User saveUser(@Valid @RequestBody User user){
         User savedUser = userService.saveUser(user);
         return  savedUser;
     }
 
-    //getting a user
+    @ApiOperation(value = "Retrieving a user profile information with the user id")
     @GetMapping("/user/{id}")
     public User getUser(@PathVariable(value = "id") int userId){
         User user = userService.getUserById(userId);
@@ -28,6 +31,7 @@ public class UserController {
     }
 
     //update a user
+    @ApiOperation(value = "Updating a user profile information with the user id")
     @PutMapping("/user/{id}")
     public User updateUser(@PathVariable(value = "id") int userId, @Valid @RequestBody User user){
         User updatedUser = userService.updateUserById(userId, user);
@@ -35,6 +39,7 @@ public class UserController {
     }
 
     //delete a user
+    @ApiOperation(value = "Deleting a user profile information with the user id")
     @DeleteMapping("/user/{id}")
     public User deleteUser(@PathVariable(value = "id") int userId){
         User user = userService.deleteById(userId);
